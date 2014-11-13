9c0205cd847205303d88544eef6456cba88bdf0b
Mateusz Bysiek <mb@mbdev.pl>
2014-11-13 14:52:43 +0900

    lab4 english and polish
    Change-Id: Ie0222fa4c66f7a169a09fdd63fbeb5a6deb8b5cf
    Signed-off-by: Mateusz Bysiek <mb@mbdev.pl>

---
+++ Programming4_Lab04_WPF/MainWindow.xaml.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Programming4_Lab04_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private GridLength GridLengthZero;
+
+        private double A, B, W, H;
+        private DispatcherTimer dt;
+        private double interval = 10;
+        private double time = 0;
+
+        ObservableCollection<Point> plotPoints = new ObservableCollection<Point>();
+        public ObservableCollection<Point> PointsOfPlot
+        { get { return plotPoints; } }
+
+        public MainWindow()
+        {
+            GridLengthZero = new GridLength(0);
+            InitializeComponent();
+
+            dt = new DispatcherTimer();
+            dt.Interval = new TimeSpan(0, 0, 0, 0, (int)interval);
+            dt.Tick += new EventHandler(dispatcherTimer_Tick);
+            dt.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            double time_old = time;
+            time += (interval / 250) * (sliderSpeed.Value / sliderSpeed.Maximum);
+            addPoint(time_old, time);
+        }
+
+        private Thickness getCanvasDims()
+        {
+            int margin = 2; //extra margin
+            return new Thickness(margin, margin,
+                canvas.ActualWidth - margin * 2, canvas.ActualHeight - margin * 2);
+        }
+
+        private void addPoint(double t0, double t)
+        {
+            double a = sliderA.Value, b = sliderB.Value;
+            double w = canvas.ActualWidth, h = canvas.ActualHeight;
+
+            if (A != a || B != b || W != w || H != h)
+            {
+                //size changed
+                PointsOfPlot.Clear();
+            }
+
+            Thickness rect = getCanvasDims();
+
+            //set this to true to check out the old drawing method
+            bool normalVariant = false;
+
+            if (normalVariant)
+            {
+                //old drawing method, does not look good at the speed of light
+                // (or close to that speed)
+
+                Double xUnit = Math.Sin(t * a), yUnit = Math.Sin(t * b);
+                double x = rect.Left + rect.Right / 2 + rect.Right / 2 * xUnit;
+                double y = rect.Top + rect.Bottom / 2 + rect.Bottom / 2 * yUnit;
+
+                PointsOfPlot.Add(new Point(x, y));
+            }
+            else
+            {
+                //after a certain treshold, the number of points per tick is increased,
+                // to compensate for the fact that at higher speeds the distance between
+                // points would be too large and the poly line would not look good
+
+                double minimum = (interval / 250) * sliderSpeed.Minimum; //0.04
+                double current = (interval / 250) * sliderSpeed.Value;
+                double maximum = (interval / 250) * sliderSpeed.Maximum; //8
+                int limit = 1 + (int)current;
+
+                double deltaT = t - t0;
+
+                //minimum: 1 point per tick, maximum: 9 points per tick
+                // (for current interface settings, the program behavior will change
+                //  according to modifications of UI)
+                for (int i = 0; i < limit; i++)
+                {
+                    double currT = t0 + ((i + 1) * deltaT) / limit;
+                    Double xUnit = Math.Sin(currT * a), yUnit = Math.Sin(currT * b);
+                    double x = rect.Left + rect.Right / 2 + rect.Right / 2 * xUnit;
+                    double y = rect.Top + rect.Bottom / 2 + rect.Bottom / 2 * yUnit;
+
+                    PointsOfPlot.Add(new Point(x, y));
+                }
+            }
+
+            if (radioButtonHome.IsChecked == true && enableFade.IsChecked == true)
+                for (int i = PointsOfPlot.Count - 1 - (int)sliderFade.Value;
+                    i >= 0;
+                    PointsOfPlot.RemoveAt(i--)) ;
+
+            PropertyChanged(this, new PropertyChangedEventArgs("PointsOfPlot"));
+
+            A = a;
+            B = b;
+            W = w;
+            H = h;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+
+    public class BooleanToGridLengthConverter : IValueConverter
+    {
+        private GridLength glZero = new GridLength(0);
+
+        public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value.GetType() == typeof(Boolean) && targetType == typeof(GridLength))
+            {
+                Boolean b = (Boolean)value;
+                if (b == true)
+                    return GridLength.Auto;
+                else
+                    return glZero;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value.GetType() == typeof(GridLength) && targetType == typeof(Boolean))
+            {
+                GridLength gl = (GridLength)value;
+                if (gl.IsAuto)
+                    return true;
+                else
+                    return false;
+            }
+            return null;
+        }
+    }
+
+    public class DoubleToStringConverter : IValueConverter
+    {
+        public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value.GetType() != typeof(Double) || targetType != typeof(String))
+                return null;
+
+            if (value == null)
+                return "?";
+
+            Double val = (double)value;
+            return Math.Round(val, 3).ToString();
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value.GetType() != typeof(String) || targetType != typeof(Double))
+                return null;
+
+            if (value == null)
+                return 0.666;
+
+            String val = (String)value;
+            return Double.Parse(val);
+            //return null;
+        }
+    }
+
+    public class PointCollectionConverter : IValueConverter
+    {
+        public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value.GetType() == typeof(ObservableCollection<Point>) && targetType == typeof(PointCollection))
+            {
+                var pointCollection = new PointCollection();
+                foreach (var point in value as ObservableCollection<Point>)
+                    pointCollection.Add(point);
+                return pointCollection;
+            }
+            return null;
+
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null; //not needed
+        }
+
+    }
+
+    public class PointCollectionForCircleConverter : IValueConverter
+    {
+        protected bool setX = true;
+
+        public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value.GetType() == typeof(ObservableCollection<Point>) && targetType == typeof(Double))
+            {
+                ObservableCollection<Point> pts = (ObservableCollection<Point>)value;
+                int len = pts.Count;
+
+                if (len > 0)
+                    if (setX)
+                        return pts.ElementAt(len - 1).X - 5;
+                    else
+                        return pts.ElementAt(len - 1).Y - 5;
+                else return 5;
+            }
+            return null;
+
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null; //not needed
+        }
+
+    }
+
+    public class PointCollectionForCircleXConverter : PointCollectionForCircleConverter
+    {
+        public PointCollectionForCircleXConverter()
+        {
+            setX = true;
+        }
+    }
+
+    public class PointCollectionForCircleYConverter : PointCollectionForCircleConverter
+    {
+        public PointCollectionForCircleYConverter()
+        {
+            setX = false;
+        }
+    }
+}
---
+++ Programming4_Lab04pl_WPF/MainWindow.xaml.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Windows.Markup;
+
+namespace Programming4_Lab04pl_WPF
+{
+	public sealed class TttField : Button
+	{
+		public TttField(Boolean black)
+		{
+			if (black)
+				this.Background = Brushes.Black;
+			else
+				this.Background = Brushes.White;
+		}
+	}
+
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		//private Ellipse redEllipse, blueEllipse;
+		//private Rectangle blackRect, whiteRect;
+		private TttField[,] handlers = null;
+		private Rectangle[,] rects = null;
+		private Ellipse[,] pieces = null;
+
+		private int index;
+		private int[] sizes;
+		private int turn;
+		private bool redStarted;
+
+		//private FrameworkElement Clone(FrameworkElement e)
+		//{
+		//    XmlDocument document = new XmlDocument();
+		//    document.LoadXml(XamlWriter.Save(e));
+
+		//    return (FrameworkElement)XamlReader.Load(new XmlNodeReader(document));
+		//}
+
+		public MainWindow()
+		{
+			//redEllipse = new Ellipse();
+			//blueEllipse = new Ellipse();
+			
+			//blackRect = new Rectangle();
+			//blackRect.Fill = Brushes.Black;
+			//whiteRect = new Rectangle();
+			//blackRect.Fill = Brushes.White;
+
+			index = -1;
+			sizes = new int[3]; //{3, 5, 9};
+			sizes[0] = 3;
+			sizes[1] = 5;
+			sizes[2] = 9;
+			turn = -1;
+			redStarted = false;
+
+			InitializeComponent();
+		}
+
+		private void optionNew_Click(object sender, RoutedEventArgs e)
+		{
+			radio3x3.IsChecked = true;
+			buttonConfirm_Click(sender, e);
+		}
+
+		private void optionExit_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		private void Rect_MouseDown(Object sender, MouseButtonEventArgs e)
+		{
+			if (turn < 0) return;
+			if (sender.GetType() == typeof(Ellipse))
+			{
+				MessageBox.Show("You can't place a piece on another piece.", "Sorry, pal...");
+				return;
+			}
+			Rectangle rect = (Rectangle)sender;
+			int x = Grid.GetColumn(rect);
+			int y = Grid.GetRow(rect);
+			if (pieces[x, y] != null)
+			{
+				MessageBox.Show("There is a piece on this field already.", "Sorry, pal...");
+				return;
+			}
+
+			pieces[x, y] = new Ellipse();
+			pieces[x, y].Fill = (turn == 0 ? Brushes.Red : Brushes.Blue);
+			gridBoard.Children.Add(pieces[x, y]);
+			Grid.SetColumn(pieces[x, y], x);
+			Grid.SetRow(pieces[x, y], y);
+			//I don't need no handlers, it's a kind of magic!
+			//pieces[x, y].MouseDown += Rect_MouseDown;
+			//pieces[x, y].MouseEnter += Ellipse_MouseEnter;
+			//pieces[x, y].MouseLeave += Ellipse_MouseLeave;
+
+			turn = (turn + 1) % 2;
+		}
+
+		//private void Ellipse_MouseEnter(Object sender, MouseEventArgs e)
+		//{
+		//    //Ellipse el = (Ellipse)sender;
+		//    ////el.Style = (Style)this.Resources.FindName("transparent");
+		//    //el.Style = new Style(typeof(Ellipse), (Style)this.Resources.FindName("transparent"));
+		//    ////Ellipse el = (Ellipse)sender;
+		//    ////Color c = new Color();
+		//    ////c = ((SolidColorBrush)el.Fill).Color;
+		//    ////c.A = 128;
+		//    ////el.Fill = new SolidColorBrush(c);
+		//    //el.op
+		//}
+
+		//private void Ellipse_MouseLeave(Object sender, MouseEventArgs e)
+		//{
+		//    //Ellipse el = (Ellipse)sender;
+		//    //Color c = new Color();
+		//    //c = ((SolidColorBrush)el.Fill).Color;
+		//    //c.A = 255;
+		//    //el.Fill = new SolidColorBrush(c);
+		//}
+
+		private void buttonConfirm_Click(object sender, RoutedEventArgs e)
+		{
+			if ((bool)radio3x3.IsChecked) index = 0;
+			if ((bool)radio5x5.IsChecked) index = 1;
+			if ((bool)radio9x9.IsChecked) index = 2;
+
+			if(index < 0) return;
+			int newsize = sizes[index];
+			turn = (redStarted ? 1 : 0);
+			if (redStarted) redStarted = false;
+			else redStarted = true;
+
+			gridBoard.ColumnDefinitions.Clear();
+			gridBoard.RowDefinitions.Clear();
+			for (int i = 0; i < newsize; i++)
+			{
+				gridBoard.ColumnDefinitions.Add(new ColumnDefinition());
+				gridBoard.RowDefinitions.Add(new RowDefinition());
+			}
+
+			rects = null;
+			rects = new Rectangle[newsize, newsize];
+			pieces = null;
+			pieces = new Ellipse[newsize, newsize];
+			handlers = null;
+			handlers = new TttField[newsize, newsize];
+			for (int x = 0; x < newsize; x++)
+			{
+				for (int y = 0; y < newsize; y++)
+				{
+					rects[x, y] = new Rectangle();
+					rects[x, y].Fill = ((x + y) % 2 == 0 ? Brushes.White.Clone() : Brushes.Black.Clone());
+
+					handlers[x, y] = new TttField((x + y) % 2 == 0);
+					handlers[x, y].MouseDown += Rect_MouseDown;
+
+					gridBoard.Children.Add(handlers[x, y]);
+					Grid.SetColumn(handlers[x, y], x);
+					Grid.SetRow(handlers[x, y], y);
+
+					handlers[x, y].Content = rects[x, y];
+
+					//MessageBox.Show(rects[x, y].Fill.ToString(), "My coat of many colours...");
+				}
+			}
+		}
+
+		private void buttonChangeNames_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
